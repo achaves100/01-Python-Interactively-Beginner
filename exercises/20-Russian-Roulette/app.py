03c1dcc30b5b54be11072b9d5c82c2162c166bc2
achaves100 <adan.chaves@outlook.com>
2021-04-11 20:38:48 +0000

    completed

--- exercises/20-Russian-Roulette/app.py
+++ exercises/20-Russian-Roulette/app.py
@@ -2,15 +2,19 @@ import random
 
 bullet_position = 3
 
+
 def spin_chamber():
-	chamber_position = random.randint(1,6)
-	return chamber_position
+    chamber_position = random.randint(1, 6)
+    return chamber_position
 
 #  DON'T CHANGE THE CODE ABOVE
+
+
 def fire_gun():
-	# YOUR CODE HERE
-
-
+    if bullet_position == spin_chamber:
+        return "You are dead!"
+    else:
+        return "Keep playing!"
 
 
 print(fire_gun())
